96c7df4111ebb4a34afb4721f39ac15528559459
ADMEN_KT13 <rgershteyn@mail.ru>
2025-10-03 18:20:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -19,16 +19,22 @@ startButton.addEventListener("click", (event) => {
 
 
 function createBoard(count, columns) {
-
   gameBoard.textContent = "";
 
+  // Создание клона шаблона
   const template = document.querySelector('#gameTableTemplate').cloneNode(true).content;
+  // В шаблоне находится таблица
   const gameTable = template.querySelector('.table');
+  // В шаблоне находится кнопка "Рестарт"
   const restartBtn = template.querySelector(".table__button");
 
-  for (let i = 0; i < count; i++) {
-    gameTable.append(createCard());
-  }
+  // Создание определенного количества иконок
+  const icons = createIconsArray(count);
+
+  // Заполнение ячеек карточками
+  icons.forEach((icon) => {
+    gameTable.append(createCard(icon));
+  });
 
   gameTable.style = `
   grid-template-columns: repeat(${columns}, 1fr);
@@ -42,9 +48,7 @@ function createBoard(count, columns) {
   });
 
   gameBoard.append(restartBtn);
-
 };
-
 
 function createCard(flippedIcon) {
   // Клонирование шаблона
@@ -53,8 +57,66 @@ function createCard(flippedIcon) {
   const card = template.querySelector('.card');
   // Добавление иконки, название которой передаем через параметр flippedIcon
   card.querySelector('#flippedIcon').classList.add(`fa-${flippedIcon}`);
-  // card.addEventListener('click', (event) => gameLogic(event, card));
 
-  // rutern card означает, что получившийся объект "выбрасывается" в то место, где будет вызвана функция createCard
+  // ruturn card означает, что получившийся объект "выбрасывается" в то место, где будет вызвана функция createCard
   return card;
+}
+
+function createIconsArray(initialCount) {
+  // Массив названий иконок
+  const cardsIcons = [
+    "compass",
+    "cloud",
+    "play",
+    "bolt",
+    "stop",
+    "cogs",
+    "atom",
+    "basketball-ball",
+    "arrows",
+    "angle-left",
+    "bars",
+    "file",
+    "filter",
+    "gear",
+    "folder",
+    "folder-open",
+    "shield",
+    "scissors",
+    "pen-clip",
+  ];
+
+  // Выбор нужного количества иконок с помощью среза
+  let cards = cardsIcons.slice(0, Math.floor(initialCount / 2));
+  // Создание пар элементов
+  const duobleCards = dublicateElements(cards);
+  // Случайное перемешивание элементов и возврат итогового массива
+  return shuffleArray(duobleCards);
+};
+
+// Перемешивание элементов массива
+function shuffleArray(array) {
+  let currentIndex = array.length;
+
+  while (currentIndex !== 0) {
+    const randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    const temp = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temp;
+  };
+
+  return array;
+}
+
+// Дублирование всех элементов входящего массива
+function dublicateElements(array) {
+  const newArr = [];
+
+  array.forEach((item) => {
+    newArr.push(item, item);
+  });
+
+  return newArr;
 }
